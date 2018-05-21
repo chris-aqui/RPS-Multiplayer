84a7c351d4036d34512c79bafda22192d1b94566
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-05-21 16:12:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Online Rock Paper Scissors game :video-game:
+
+* Find a friend to play with
+* Share link
+* Enter name
+* follow instructions
+
+| :trophy: | :video-game: | :trophy: |
---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAu-uybDqc5UnLbfS0UUiuBKYxttRfYSUQ",
+    authDomain: "rps-multiplayer-ca.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-ca.firebaseio.com",
+    projectId: "rps-multiplayer-ca",
+    storageBucket: "rps-multiplayer-ca.appspot.com",
+    messagingSenderId: "147823995701"
+  };
+  firebase.initializeApp(config);
