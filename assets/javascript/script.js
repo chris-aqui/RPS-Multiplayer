48bec7741ac2565affd55c2f4607a64993130c89
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-05-24 21:13:08 -0400

    at this point adding a user name will upload to firebase and then display a user iamge

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,26 @@
   // var
-  var players1 = {
-    number: '0',
-    name: name,
-    wins: 0,
-    losses: 0,
-    turns: 0,
-    choice: ''
-    // player a
-  };
-  var players2 = {
-    number: '0',
-    name: name,
-    wins: 0,
-    losses: 0,
-    turns: 0,
-    choice: ''
-    // player a
-  };
+  var players = [{
+      // player1
+      number: '0',
+      name: 'Player Unknown',
+      wins: 0,
+      losses: 0,
+      turns: 0,
+      choice: '',
+      image:'assets/image/Player1.png'
+    },
+    {
+      // player2
+      number: '0',
+      name: 'Player Unknown',
+      wins: 0,
+      losses: 0,
+      turns: 0,
+      choice: '',
+      image:'assets/image/player2.png'
+    }
+
+  ];
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyAu-uybDqc5UnLbfS0UUiuBKYxttRfYSUQ",
@@ -28,9 +32,20 @@
   };
   firebase.initializeApp(config);
   var database = firebase.database();
+
+  var data;
+
+  database.ref().on("value", function(snapshot) {
+    console.log(snapshot.val());
+    data = snapshot.val();
+    // console.log(data[0].image);
+  });
   //
   document.addEventListener('DOMContentLoaded', function () {
     // on webpage loaded
+    document.getElementById('userHNameA').innerHTML = players[0].name;
+    document.getElementById('userHNameB').innerHTML = players[1].name;
+    database.ref().set(players);
     database.ref().on('value', function (snapshot) {
       console.log(snapshot.val());
     });
@@ -39,14 +54,17 @@
   document.querySelector('#userNameA').addEventListener('keypress', function (e) {
     var key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
-      players1.name = document.querySelector('#userNameA').value; //gets the username
-      console.log(players1.name); // console her
-      document.querySelector('#userHNameA').innerHTML = players1.name; // push the username to the display
+      players[0].name = document.querySelector('#userNameA').value; //gets the username
+      console.log(players[0].name); // console her
+      document.querySelector('#userHNameA').innerHTML = players[0].name; // push the username to the display
       document.getElementById('userNameA').style.visibility = 'hidden';
       //
-      database.ref().set({
-        userA: players1.name,
+      database.ref(0).update({
+        name: players[0].name,
       });
+      if( players[0].name != 'Player Unknown'){
+        document.querySelector("#p1Image").setAttribute('src', players[0].image);
+      }
     }
   });
 
@@ -54,14 +72,18 @@
   document.querySelector('#userNameB').addEventListener('keypress', function (e) {
     var key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
-      players2.name = document.querySelector('#userNameB').value; //gets the username
-      console.log(players2.name); // console her
-      document.querySelector('#userHNameB').innerHTML = players2.name; // push the username to the display
+      players[1].name = document.querySelector('#userNameB').value; //gets the username
+      console.log(players[1].name); // console her
+      document.querySelector('#userHNameB').innerHTML = players[1].name; // push the username to the display
       document.getElementById('userNameB').style.visibility = 'hidden';
       //
-      database.ref().set({
-        userB: players1.name,
+      database.ref(1).update({
+        name: players[1].name,
       });
+      //
+      if( players[1].name != 'Player Unknown'){
+        document.querySelector("#p2Image").setAttribute('src', players[1].image);
+      }
     }
   });
 
