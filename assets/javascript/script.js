f7ab877351cf2869af9004143a36800e9bf14678
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-05-27 20:59:40 -0400

    test firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,7 +7,7 @@
       losses: 0,
       turns: 0,
       choice: '',
-      image:'assets/image/Player1.png'
+      image: 'assets/image/Player1.png'
     },
     {
       // player2
@@ -17,7 +17,7 @@
       losses: 0,
       turns: 0,
       choice: '',
-      image:'assets/image/player2.png'
+      image: 'assets/image/player2.png'
     }
 
   ];
@@ -35,20 +35,26 @@
 
   var data;
 
-  database.ref().on("value", function(snapshot) {
-    console.log(snapshot.val());
+  database.ref().on("value", function (snapshot) {
+    // console.log(snapshot.val());
     data = snapshot.val();
-    // console.log(data[0].image);
+    players = snapshot.val().players;
+
+    // console.log('my data is ' + data);
   });
   //
   document.addEventListener('DOMContentLoaded', function () {
-    // on webpage loaded
+    // At the initial load and on subsequent data value changes,
+    // get a snapshot of the current data. (I.E FIREBASE HERE)
+    // This callback keeps the page updated when a value changes in firebase.
+    database.ref().on('value', function (snapshot) {
+      // Change the values to match the value in the database
+      players = snapshot.val();
+      console.log('My snapshot ' + players);
+    });
+    //
     document.getElementById('userHNameA').innerHTML = players[0].name;
     document.getElementById('userHNameB').innerHTML = players[1].name;
-    database.ref().set(players);
-    database.ref().on('value', function (snapshot) {
-      console.log(snapshot.val());
-    });
   });
   //player 1
   document.querySelector('#userNameA').addEventListener('keypress', function (e) {
@@ -56,13 +62,15 @@
     if (key === 13) { // 13 is enter
       players[0].name = document.querySelector('#userNameA').value; //gets the username
       console.log(players[0].name); // console her
-      document.querySelector('#userHNameA').innerHTML = players[0].name; // push the username to the display
+      document.querySelector('#userHNameA').innerHTML = players[0].name; // push the username
+      document.querySelector('#userHNameB').innerHTML = players[1].name; // push the username to the display
       document.getElementById('userNameA').style.visibility = 'hidden';
       //
       database.ref(0).update({
         name: players[0].name,
       });
-      if( players[0].name != 'Player Unknown'){
+      //
+      if (players[0].name != 'Player Unknown') {
         document.querySelector("#p1Image").setAttribute('src', players[0].image);
       }
     }
@@ -72,16 +80,19 @@
   document.querySelector('#userNameB').addEventListener('keypress', function (e) {
     var key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
+      //
       players[1].name = document.querySelector('#userNameB').value; //gets the username
       console.log(players[1].name); // console her
-      document.querySelector('#userHNameB').innerHTML = players[1].name; // push the username to the display
+      //
+      document.querySelector('#userHNameB').innerHTML = players[1].name; // push the username
+      document.querySelector('#userHNameA').innerHTML = players[0].name; // update
       document.getElementById('userNameB').style.visibility = 'hidden';
       //
       database.ref(1).update({
         name: players[1].name,
       });
       //
-      if( players[1].name != 'Player Unknown'){
+      if (players[1].name != 'Player Unknown') {
         document.querySelector("#p2Image").setAttribute('src', players[1].image);
       }
     }
@@ -99,3 +110,10 @@
   // });
   //
   // Game Logic
+
+
+
+      // on webpage loaded
+    // set firebase
+    // database.ref().update(players);
+    //
